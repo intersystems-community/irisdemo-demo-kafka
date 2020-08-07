f77ac6b8efcd93e8e23850f4f917bf4068b8cdd3
Amir Samary <amirsamary@gmail.com>
2020-08-06 23:35:23 -0400

    Bump version to 1.2.1.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+## 1.2.1 (August 06, 2020)
+  - Adding missing kafka adapter
+
 ## 1.2.0 (August 06, 2020)
  - Initial version
--- runiris.sh
+++ runiris.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name iris-data-sink --rm --init -p 52773:52773 -p 51773:51773 intersystemsdc/irisdemo-demo-restm2:iris-data-sink-version-1.2.0
+docker run --name iris-data-sink --rm --init -p 52773:52773 -p 51773:51773 intersystemsdc/irisdemo-demo-restm2:iris-data-sink-version-1.2.1
